c30b482c0bfdb516cbe0bc47584757f479ec9d4e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-11-03 11:36:22 +0000

    string convert method

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,26 +2,27 @@ import java.util.stream.*;
 
 public class StringCalculator {
 
-  public int add(String numbers){
-    if(empty(numbers))
-      return 0;
-    int digits = stringConvertor(numbers);
+  private int[] digits;
 
+  public int add(String numbers){
+    // if(empty(numbers))
+
+      return 0;
   }
 
   private boolean empty(String numbers){
     return numbers == "";
   }
 
-  private int stringConvertor(String numbers){
+
+
+  public int[] stringConvertor(String numbers){
     String[] numbersString = numbers.split("\\s*(=>|,|\\s)\\s*");
-
-
-    for (int i= 0; i < numbersString.length; i++){
-      System.out.println(numbersString[i]);
+    digits = new int[numbersString.length];
+    for (int digit = 0; digit < numbersString.length; digit++){
+      digits[digit] = Integer.parseInt(numbersString[digit]);
     }
-
-    return Integer.parseInt(numbersString);
+    return digits;
   }
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,5 +1,11 @@
 import junit.framework.TestCase;
 
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+
+
+
 public class StringCalculatorTest extends TestCase {
    private StringCalculator calc;
 
@@ -16,9 +22,15 @@ public class StringCalculatorTest extends TestCase {
   //   assertEquals(1, calc.add("1"));
   // }
 
-  public void testTakesTwoNumbers() throws Exception {
-    assertEquals(3, calc.add("1, 2"));
+  // public void testTakesTwoNumbers() throws Exception {
+  //   assertEquals(3, calc.add("1, 2"));
+  // }
+
+  public void testStringConverter() throws Exception {
+      int[] expectedArray = {1,2};
+      assertArrayEquals(expectedArray, calc.stringConvertor("1,2"));
   }
+
 
 
 }
