351c05939404eedd24aed106772e548e26a68ff2
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-11-03 17:35:06 +0000

    delimeter test failing refactoring of string converter method in progress

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,28 +4,36 @@ import java.util.Arrays;
 public class StringCalculator {
 
   private int[] digits;
+  // set a boolean if delilmiter??
 
   public int add(String numbers){
-    if(empty(numbers))
+    if(isEmpty(numbers))
     return 0;
+    // System.out.println(numbers);
       stringConvertor(numbers);
     if(isNegative(digits))
       throw new IllegalArgumentException("No Negative numbers");
-    digits = removeAboveOneThousand(digits);  
+    digits = removeAboveOneThousand(digits);
     int total = totaller(digits);
     return total;
   }
 
-  private boolean empty(String numbers){
+  private boolean isEmpty(String numbers){
     return numbers == "";
   }
 
   public int[] stringConvertor(String numbers){
+      // System.out.println(numbers);
+
+      // extract this to its own method for single respons and also to check for delims before
+      // it makes an int array (method doing spliting and inting )
     String[] numbersString = numbers.split("\\s*(=>|,|\\s)\\s*");
+    // System.out.println(numbersString[0]);
     digits = new int[numbersString.length];
     for (int digit = 0; digit < numbersString.length; digit++){
       digits[digit] = Integer.parseInt(numbersString[digit]);
     }
+    System.out.println(digits[0]);
     return digits;
   }
 
@@ -43,6 +51,10 @@ public class StringCalculator {
     int[] removed = Arrays.stream(digits)
       .filter(e -> e < 1000).toArray();
     return removed;
+  }
+
+  public boolean isDelimeter (String numbers){
+    return true;
   }
 
 
